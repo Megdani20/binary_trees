3623f0787ef50439b82a17e819f170200c7bc7cf
Soukaina <megdani20soukaina@gmail.com>
2024-06-08 01:02:58 +0100

    Update Binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,5 +6,18 @@
  * @tree: - is a pointer to the parent node of the node to create
  * Return: a pointer to the newnode node, or NULL on failure
  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0;
+	size_t height_right = 0;
+
+	if (!tree)
+		return (0);
+
+	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_left > height_right ? height_left : height_right);
+}
+
 
 
